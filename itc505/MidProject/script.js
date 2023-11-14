35c93aea4cbbdd192fd73ac98cbb7068aa88c257
mounika-anumala <144751212+mounika-anumala@users.noreply.github.com>
2023-11-13 23:29:34 -0700

    Add files via upload

---
+++ itc505/MidProject/script.js
@@ -0,0 +1,60 @@
+const startPage = {
+
+    story: "You finished your schooling. Choose a next step for continuing studies.",
+    buttons: ["Intermediate","Diploma"],
+    images:["./images/intermediate.jpg","./images/diploma.jpg"]
+
+}
+
+const afterDiploma = {
+
+    story: "You successfully completed your diplamo course, choose the below options for your next step.",
+    buttons: ["Bachelors","Job"],
+    images: ["./images/bachelors.avif","./images/IT.png"]
+
+}
+
+const afterInter = {
+
+    story: "You chose to do intermediate. Select if you like to do maths or biology.",
+    buttons: ["Mathematics","Biology"],
+    images: ["./images/math.jpg","./images/bio.jpg"]
+
+}
+
+const afterMath = {
+
+    story: "you finished your intermediate education in maths. Choose a next step for your studies.",
+    buttons: ["Commerce","Bachelors"],
+    images: ["./images/commerce.png","./images/bachelors.avif"]
+
+}
+
+const afterBio = {
+
+    story: "you finished your intermediate education in Biology. Choose a next step for your studies.",
+    buttons: ["Medicine","Agri Science"],
+    images: ["./images/medicine.jpg","./images/agricultural.webp"]
+
+}
+
+const afterComm = {
+
+    story: "You sucessfully finished commerce, fly high with your new skill.",
+    image: ""
+
+}
+
+const afterBach={
+
+    story: "You finished your bachelors, you can earn high or study high now. Choose an option.",
+    buttons:["Masters","Job"],
+    images: ["./images/masters.png","./images/IT.png"]
+}
+
+const afterMedi = {
+
+    story: "You finished your medicine studies. You can now become a physician or do a specialization",
+    buttons: ["",""]
+
+}
